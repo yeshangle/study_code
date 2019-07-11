4180a6de9dbd69b0961609e4859bfab4f1589085
yeshangle <852403278@qq.com>
2019-07-11 11:54:20 +0800

    test

--- study_code/task.py
+++ study_code/task.py
@@ -4,7 +4,9 @@ from celery import Celery
 
 # app = Celery('task', broker='redis://localhost/0',
 #                    backend='redis://localhost/0')
-app = Celery('tasks', broker='amqp://guest@localhost//')
+# app = Celery('tasks', broker='amqp://guest@localhost//')
+app = Celery('tasks', backend='amqp', broker='amqp://')
+
 
 @app.task
 def add(x, y):
