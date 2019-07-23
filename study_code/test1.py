9a5d571a4dde249ed8dc7414a43813c7920050ba
yeshangle <852403278@qq.com>
2019-07-23 15:25:48 +0800

    test

---
+++ study_code/test1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from redis import StrictRedis
+
+redis = StrictRedis(host='localhost', port=6379)
+
+redis.zadd("end_game_world_rank", 21, 1003856)
+
+redis.zadd("end_game_world_rank", 19, 1003853)
+redis.zadd("end_game_world_rank", 11, 1003857)
+redis.zadd("end_game_world_rank", 25, 1003859)
+redis.zadd("end_game_world_rank", 28, 1003860)
+
+
+ret_data = redis.zrevrange("end_game_world_rank", 0, -1, withscores=True)
+
+print ret_data
+
