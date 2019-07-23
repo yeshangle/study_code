dbf4ad0ef45cb723c3d384af6aab77c6e20c86db
yeshangle <852403278@qq.com>
2019-07-23 15:36:29 +0800

    test

--- study_code/test1.py
+++ study_code/test1.py
@@ -3,12 +3,12 @@ import redis
 
 redis = redis.Redis(host='localhost', port=6379)
 
-redis.zadd("end_game_world_rank", "21", "1003856")
+redis.zadd("end_game_world_rank", level=21, uin=1003856)
 
-redis.zadd("end_game_world_rank", "19", "1003853")
-redis.zadd("end_game_world_rank", "11", "1003857")
-redis.zadd("end_game_world_rank", "25", "1003859")
-redis.zadd("end_game_world_rank", "28", "1003860")
+redis.zadd("end_game_world_rank", level=19, uin=1003853)
+redis.zadd("end_game_world_rank", level=11, uin=1003857)
+redis.zadd("end_game_world_rank", level=25, uin=1003859)
+redis.zadd("end_game_world_rank", level=28, uin=1003860)
 
 
 ret_data = redis.zrevrange("end_game_world_rank", 0, -1, withscores=True)
