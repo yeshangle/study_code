9a391880b75537752b1aa2b4049857724d9133a0
yeshangle <852403278@qq.com>
2019-09-15 09:48:55 +0800

    爬虫

---
+++ study_code/tutorial/tutorial/spiders/mingyan_spider.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class MngYan(scrapy.Spider):
+    """
+    用于爬取相关数据
+    """
+
+    # 蜘蛛名字
+    name = "mingyan"
+
+    def start_requests(self):
+
+        # 爬取的连接
+        urls = [
+            'http://lab.scrapyd.cn/page/1/',
+            'http://lab.scrapyd.cn/page/2/',
+        ]
+
+        for url in urls:
+            yield  scrapy.Request(url=url, callback=self.parse())
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = "mingyan-%s.html" % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log("保存文件：%s" % filename)
+
