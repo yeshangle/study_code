b5ccb59b56571fc939a3c5f272ce72a5208bd008
yeshangle <852403278@qq.com>
2019-09-16 15:15:25 +0800

    debug

--- study_code/yilan_spider/yilan_spider/spiders/yilan.py
+++ study_code/yilan_spider/yilan_spider/spiders/yilan.py
@@ -12,9 +12,13 @@ class MngYan(scrapy.Spider):
     def start_requests(self):
 
         # 爬取的连接
-        mm = "./test.html"
-        self.log("mm is %s" % mm)
-        return mm
+        urls = [
+            'http: // www.job1001.com / SearchResult.php?page = 0 & sums = & & parentName = & key = & region_1 = & region_2 = & region_3 = & keytypes = & jtzw = % D2 % BB % BC % B6 % BD % A8 % D4 % EC % CA % A6 & data = & dqdh_gzdd = & jobtypes = & edus = & titleAction = & provinceName = & sexs = & postidstr = & postname = & searchzwtrade = & gznum = & rctypes = & salary = & showtype = list & sorttype = score'
+        # main_search
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
 
     def parse(self, response):
         filename = "yilan.html"
