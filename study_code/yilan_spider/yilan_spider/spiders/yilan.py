fd711c86f8dc4a81f362811f5dfa0963b86c3ff9
yeshangle <852403278@qq.com>
2019-09-18 12:18:14 +0800

    debug

--- study_code/yilan_spider/yilan_spider/spiders/yilan.py
+++ study_code/yilan_spider/yilan_spider/spiders/yilan.py
@@ -19,6 +19,6 @@ class MngYan(scrapy.Spider):
     def parse(self, response):
         filename = "yilan.html"
         with open(filename, 'w') as f:
-            f.write(response.body.decode('utf-8'))
+            f.write(response.body.encode('gb2312').decode('utf-8'))
         self.log("保存文件：%s" % filename)
 
