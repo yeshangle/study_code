25825cb091ccf99dc708dbf4dc50938c43c47abf
yeshangle <852403278@qq.com>
2019-09-18 20:56:42 +0800

    debug

--- study_code/tutorial/tutorial/settings.py
+++ study_code/tutorial/tutorial/settings.py
@@ -22,7 +22,6 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 ROBOTSTXT_OBEY = True
 
 
-FEED_EXPORT_ENCODING = 'GB2312'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
