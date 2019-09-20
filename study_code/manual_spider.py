85e4fd7bfd14c54e2ebab5bb25aca9de46a28274
yeshangle <852403278@qq.com>
2019-09-20 17:52:34 +0800

    debug

--- study_code/manual_spider.py
+++ study_code/manual_spider.py
@@ -53,8 +53,8 @@ class DataSpider(object):
         # 职位要求
         order_list = list()
         opsition_info = soup.find_all(attrs={"class": "job_depict"})
-        for info in opsition_info:
-            order_list.append(info.content)
+        for info in opsition_info.children:
+            order_list.append(info)
 
         print company_name, position_name, order_list
 
