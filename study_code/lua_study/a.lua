63bc8f3f0b60fab714c971333ba161566035a4c1
yeshangle <852403278@qq.com>
2020-01-04 15:51:17 +0800

    dbeug

--- study_code/lua_study/a.lua
+++ study_code/lua_study/a.lua
@@ -1,1 +1,11 @@
-print ("hello world lua")
+function fact(n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n-1)
+    end
+end
+
+print ("enter a number: ")
+a = io.read("*number")
+print(fact(a))
