e80d7295db9e626d4b33fc0ef4a0689429557a3b
yeshangle <852403278@qq.com>
2020-01-05 10:18:29 +0800

    debug

--- study_code/lua_study/a.lua
+++ study_code/lua_study/a.lua
@@ -1,1 +1,22 @@
-print ("hello world lua")
+--function fact(n)
+--    if n == 0 then
+--        return 1
+--    else
+--        return n * fact(n-1)
+--    end
+--end
+--
+--
+--a = io.read("*n")
+--print("enter a number:")
+--
+--print(fact(a))
+
+
+function norm(x, y)
+    return math.sqrt(x^2 + y^2)
+end
+
+function twice(x)
+    return 2.0*x
+end
